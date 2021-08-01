f50bceed75f857d54817b004d5f64d7fbb699caf
alex9849 <alexander@liggesmeyer.net>
2021-08-01 21:32:55 +0200

    First PersonCard additions

--- src/components/PersonCard.vue
+++ src/components/PersonCard.vue
@@ -12,10 +12,12 @@
           <v-img src="../assets/ALiggesmeyer-Profil.jpg"></v-img>
         </v-avatar>
       </v-col>
-      <v-col cols="8">
-        <v-card-title class="text-h5 text--primary pa-0 justify-center">
+      <v-col cols="8" class="pa-2" align="left">
+        <v-card-title class="text-h5 text--primary pa-0"
+        >
           Alexander Liggesmeyer
         </v-card-title>
+        <v-card-subtitle style="color: #666666">Student</v-card-subtitle>
       </v-col>
 
     </v-row>
