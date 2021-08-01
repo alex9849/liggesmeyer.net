4c5936d356b3b8cf9825a62e350d02a058d831cb
alex9849 <alexander@liggesmeyer.net>
2021-08-01 20:47:44 +0200

    Added Logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,78 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+            :src="require('../assets/ALiggesmeyer.png')"
+            class="my-3"
+            contain
+            height="100"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Liggesmeyer.net
+        </h1>
+      </v-col>
+
+      <v-col
+          class="mb-5"
+          cols="12"
+      >
+        <v-card>
+          <v-card-text>Test</v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col
+          class="mb-5"
+          cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+              v-for="(link, i) in importantLinks"
+              :key="i"
+              :href="link.href"
+              class="subheading mx-3"
+              target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+          class="mb-5"
+          cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+              v-for="(eco, i) in ecosystem"
+              :key="i"
+              :href="eco.href"
+              class="subheading mx-3"
+              target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    name: 'Home'
   }
 </script>
